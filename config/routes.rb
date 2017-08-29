d5d30df5723b7ad618fb5a7a37dbb0fac964c72c
Calvin-Seidle <seidlecalvin@gmail.com>
2017-08-29 16:31:32 +0200

    added blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  	
+  resources :posts
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :replies
   resources :tickets
-  root to: 'tickets#index'
+  root to: 'home#index'
   get 'home/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829125621) do
+ActiveRecord::Schema.define(version: 20170829140833) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.string "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "replies", force: :cascade do |t|
     t.integer "ticket_id"
